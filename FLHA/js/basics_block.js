a0587390162701b0d42dfb9792233123a51ade02
sdbishop98 <78835233+sdbishop98@users.noreply.github.com>
2024-08-09 10:47:28 -0600

    continued working on basics block

--- FLHA/js/basics_block.js
+++ FLHA/js/basics_block.js
@@ -25,11 +25,20 @@ function basics_html(){
     const cell_time_label = row2.insertCell();
     const cell_time_input = row2.insertCell();
     // location
-    const cell_location_label = row1.insertCell();
-    const cell_location_input = row1.insertCell();
+    make_textInput_tableHtml(row1, 'location', 'Location');
     // job number
-    const cell_jobNum_label = row2.insertCell();
-    const cell_jobNum_input = row2.insertCell();
+    make_numericInput_tableHtml(row2, 'jobNumber', 'Job Number');
+    // work affected by weather
+    const cell_weatherAffected_question = row1.insertCell();
+    cell_weatherAffected_question.rowSpan = 2;
+    const cell_weatherAffected_yes = row1.insertCell();
+    const cell_weatherAffected_no = row2.insertCell();
+    // current weather
+    const cell_currentWeather_label = row1.insertCell();
+    const cell_currentWeather_input = row1.insertCell();
+    // current temperature
+    const cell_currentTemp_label = row2.insertCell();
+    const cell_currentTemp_input = row2.insertCell();
 
     // CREATE TABLE CONTENT
     // date and time
@@ -38,49 +47,142 @@ function basics_html(){
     cell_date_input.appendChild(dateTime.date.input);
     cell_time_label.appendChild(dateTime.time.label);
     cell_time_input.appendChild(dateTime.time.input);
-    // location
-    const location = makeLocationHtml();
-    cell_location_label.appendChild(location.label);
-    cell_location_input.appendChild(location.input);
-    // job number
-    const jobNum = makeJobNumberHtml();
-    cell_jobNum_label.appendChild(jobNum.label);
-    cell_jobNum_input.appendChild(jobNum.input);
+    // work affected by weather
+    cell_weatherAffected_question.textContent = 'Is the work affected by the weather?';
+    cell_weatherAffected_question.style.maxWidth = '100px';
+    const weatherAffected_yesNo = ['Yes', 'No'];
+    const weatherAffected = makeRaioInputLabelPairs('weather-affected', weatherAffected_yesNo, weatherAffected_yesNo);
+    cell_weatherAffected_yes.appendChild(weatherAffected[0].input);
+    cell_weatherAffected_yes.appendChild(weatherAffected[0].label);
+    cell_weatherAffected_no.appendChild(weatherAffected[1].input);
+    cell_weatherAffected_no.appendChild(weatherAffected[1].label);
+    // weather conditions;
+
+
 
 }
 
-function makeLocationHtml(){
-    // Helper function for basics_html
-    // creates job location input element with corresponing label
-    // does not place these elements on the graph
-    // INPUT:   none
-    // RETURN:  tuple - location input/label pair
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.id = 'input-location';
+function make_textInput_tableHtml(row, identifier, label_str){ // done
+    // Helper function
+    // creates html table cell elements for a text input with corresponding label
+    // INPUT:   row - tr element - row to place the cell in
+    //          identifier - string - unique identifier
+    //          label_str - string - text for the label element
+    // RETURN:  none
+    const cell_label = row.insertCell();
+    const cell_input = row.insertCell();
 
-    const label = document.createElement('label');
-    label.textContent = 'Location:';
-    label.htmlFor = input.id;
+    const content = makeTextInputLabelPair(identifier, label_str);
+    cell_label.appendChild(content.label);
+    cell_input.appendChild(content.input);
 
-    return {input: input, label: label};
+    function makeTextInputLabelPair(identifier, label_str){
+        // Helper function for make_textInput_tableHtml
+        // creates text input element with corresponing label
+        // does not place these elements on the graph
+        // INPUT:   identifier - string - unique identifier
+        //          label_str - string - text for the label element
+        // RETURN:  tuple - location input/label pair
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.id = `input-text-${identifier}`;
+        input.classList.add('text');
+    
+        const label = document.createElement('label');
+        label.textContent = label_str;
+        label.htmlFor = input.id;
+    
+        return {input: input, label: label};
+    }
 }
 
-function makeJobNumberHtml(){
-    // Helper function for basics_html
-    // creates job number input element with corresponing label
+function make_numericInput_tableHtml(row, identifier, label_str, unit = null) { // done
+    // Helper function
+    // creates html table cell elements for a text input with corresponding label
+    // INPUT:   row - tr element - row to place the cell in
+    //          identifier - string - unique identifier
+    //          label_str - string - text for the label element
+    //          unit - string - optional, will place a unit at the end of the number input
+    // RETURN:  none
+    const cell_label = row.insertCell();
+    const cell_input = row.insertCell();
+
+    const content = makeNumericInputLabelPair(identifier, label_str, unit);
+    cell_label.appendChild(content.label);
+    cell_input.appendChild(content.input);
+
+    function makeNumericInputLabelPair(identifier, label_str, unit_str=null) {
+        // Helper function for make_numberInput_tableHtml
+        // creates number input element with corresponing label
+        // does not place these elements on the graph
+        // INPUT:   identifier - string - unique identifier
+        //          label_str - string - text for the label element
+        //          unit - string - optional, will place a unit at the end of the number input
+        // RETURN:  tuple - location input/label pair
+        const input = document.createElement('input');
+        input.type = 'text';
+        input.id = `input-number-${identifier}`;
+        input.inputMode = 'numeric';
+        input.pattern = '[0-9]*'
+        input.classList.add('text', 'numeric');
+
+        input.addEventListener('input', e => {
+            if(isNaN(input.value)){
+                input.classList.add('error');
+            } else if (input.classList.contains('error')){
+                input.classList.remove('error');
+            }
+        });
+
+        const label = document.createElement('label');
+        label.textContent = label_str;
+        label.htmlFor = input.id;
+
+        if(unit_str){
+            const unit = document.createElement('label');
+            unit.appendChild(input);
+            const unit_text = document.createElement('span');
+            unit_text.textContent = unit_str;
+            return {input: unit, label: label};
+        } 
+        return {input: input, label: label}
+    }
+}
+
+function validateNumericInputs() { // wip
+    const inputs = document.querySelectorAll('input.numeric');
+}
+
+
+function makeRaioInputLabelPairs(name, identifiers, labels){
+    // Helper function
+    // creates radio input elements with corresponing label
+    // all created elements will share the same name
     // does not place these elements on the graph
-    // INPUT:   none
-    // RETURN:  tuple - job number input/label pair
-    const input = document.createElement('input');
-    input.type = 'text';
-    input.id = 'input-jobNumber';
+    // INPUT:   name - string - unique name for the radio elements
+    //          identifiers - list - strings - unique identifiers
+    //          labels - list - strings - text for the label element
+    //      identifiers and labels must be the same length
+    // RETURN:  list - tuples - location input/label pair
+    //              will have the same length and order of identifiers
+    if((typeof identifiers !== 'object') || (typeof identifiers !== typeof labels) || (identifiers.length !== labels.length)){
+        throw new Error('invalid input');
+    }
+    let radios = []
+    identifiers.forEach((identifier_str, index) => {
+        const input = document.createElement('input');
+        input.type = 'radio';
+        input.id = `input-radio-${identifier_str}`;
+        input.classList.add('radio');
+        input.name = name;
 
-    const label = document.createElement('label');
-    label.textContent = 'Job Number:';
-    label.htmlFor = input.id;
+        const label = document.createElement('label');
+        label.textContent = labels[index];
+        label.htmlFor = input.id;
 
-    return {input: input, label: label};
+        radios.push({input: input, label: label});
+    })
+    return radios;
 }
 
 function makeDateTimeHtml(){
@@ -107,6 +209,7 @@ function makeDateHtml(now){
     input.type = 'date';
     input.id = 'input-date';
     input.value = now.toISOString().split('T')[0];
+    input.classList.add('date', 'dateTime');
 
     const label = document.createElement('label');
     label.textContent = 'Date:';
@@ -128,6 +231,7 @@ function makeTimeHtml(now){
     input.type = 'time';
     input.id = 'input-time';
     input.value = `${hours}:${minutes}`;
+    input.classList.add('time', 'dateTime');
 
     const label = document.createElement('label');
     label.textContent = 'Time:';
